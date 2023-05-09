56d97f5f4587c6f71ae7d0449a71ff68b697ff5d
diegorojas89z <rojasdiego.89@gmail.com>
2023-05-08 21:45:57 -0300

    Backend de API para argentinaprograma

---
+++ src/main/java/com/backendporfolio/drojas/DrojasApplication.java
@@ -0,0 +1,13 @@
+package com.backendporfolio.drojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrojasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrojasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendporfolio/drojas/DrojasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendporfolio.drojas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrojasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
